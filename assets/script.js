14d6680f4abe69267f80bbc1e1e30b045614f7ae
AMess33 <amess3309@gmail.com>
2023-01-25 18:50:08 -0600

    add psuedo code to js for api calls

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,10 @@
-// add javascript here
 
 // cat API Key live_DumA8PS0G2lXkKvpVOVCuqMLTBJB0qi7ZuWTd6Tf4wqEPwHCDLOZxjHSzdx3UU5p
 
 // create api call for dog images
     // fetch dog images
+        // append images onto html webpage
+
+// create api call for cat images
+    // fetch cat images
         // append images onto html webpage
