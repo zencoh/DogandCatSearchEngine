6015fd352da97b4ea897b3d44bcbc49b9fc82376
AMess33 <amess3309@gmail.com>
2023-01-25 20:10:18 -0600

    dog api fetch working and image src is saved as imageUrl

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,26 @@
 
 // cat API Key live_DumA8PS0G2lXkKvpVOVCuqMLTBJB0qi7ZuWTd6Tf4wqEPwHCDLOZxjHSzdx3UU5p
 
-// create api call for dog images
-    // fetch dog images
-        // append images onto html webpage
+// create api call for dog images X
+    // fetch dog images X
+        // append images onto html webpage 
+
+function dogApi(value) {
+    var requestUrl = 'https://dog.ceo/api/breed/' + value + '/images/random';
+    console.log(requestUrl);
+    fetch(requestUrl)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            imageUrl = data.message;
+        })
+// get elmement by iD
+// append image to webpage
+
+}       
+
+dogApi('hound');
 
 // create api call for cat images
     // fetch cat images
