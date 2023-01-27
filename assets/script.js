9548f9fdeac01d7a3c0df0e15e298a36efd0f1f8
Cole Hartkopp <chsoccer4@outlook.com>
2023-01-26 19:30:52 -0600

    removing Wiki API, using more data from cat and dog API

--- assets/script.js
+++ assets/script.js
@@ -40,35 +40,3 @@ function catApi(value) {
     })
 }
         catApi('beng');
-
-// bengal cat is just a placeholder until I can use the input from the HTML doc
-var breedSearched = 'bengal cat'
-// i might use something like this when a button is made, then it is taking the search term and adding it dynamically to the search url
-// let searchUrl = 'https://en.wikipedia.org/w/api.php?action=parse&origin=*&format=json&generator=search&gsrnamespace=0&gsrlimit=5&gsrsearch='
-
-// let userInput;
-// function description() {
-//     userInput = select('#userInput');
-//     userInput.changed(wikiSearch);
-//     wikiSearch();
-// }
-
-// function wikiSearch() {
-//     let term = userInput.value();
-//     let url = searchUrl + term;
-//     console.log(term);
-// }
-
-fetch("https://en.wikipedia.org/w/api.php?action=query&titles=bengal_(cat)&prop=revisions&rvprop=content&format=json")
-  .then(response => response.json())
-  .then(data => {
-    console.log(data);
-    // extract the information you need from the data here
-  })
-
-// I am continuing to do research on this API to find the right parameters for, I'm assuming the text content would help with the articles summary of the breed.
-fetch('https://www.mediawiki.org/w/api.php?action=parse&format=json&page=' + breedSearched)
-  .then((response) => response.json())
-  .then((data) => console.log(data));
-//   i will make a if then statement to display the info from the fetch request to the HTML doc once there is an HTML semantic or tag I can use to display data
-  
