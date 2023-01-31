3bdbe336b79326ccd7780029d21971d2a30ac369
AMess33 <amess3309@gmail.com>
2023-01-30 20:43:16 -0600

    create event listeners and add function calls on change

--- assets/script.js
+++ assets/script.js
@@ -3,7 +3,7 @@
 // var selectedBreed = document.getElementById('breed').value;
 // create api call for dog images X
     // fetch dog images X
-        // append images onto html webpage 
+        // append images onto html webpage X
 
 function dogImage(value) {
     // value is input from html of what breed the user selects from drop down menu
@@ -35,11 +35,8 @@ function dogApi(value) {
         return response.json();
     })
     .then(function (data) {
-        console.log(data);
        var dogAttitude = data.temperament;
-       console.log(dogAttitude);
        var dogLife = data.life_span;
-       console.log(dogLife);     
        var infoContainer = document.getElementById("infoContainer");
        infoContainer.classList.add("dog-info");
        var attitudePara = document.createElement("p");
@@ -52,23 +49,24 @@ function dogApi(value) {
     })
     
 }
-function onSelectChange(event) {
+function onDogChange(event) {
     dogApi(event.target.value);
 }
 dogApi(document.getElementById('breed').value);
 
+function onCatChange(event) {
+    catPicture(event.target.value);
+    catInfo(event.target.value);
+}
 
 // return response.textContent = 'information we want to pull from dog API'
 // need to make it so when the data is retrieved it is displayed at the bottom of the HTML doc, line 99 textArea replace placeholder
 // use local storage to store information that is retrieved from dog API. 
 // localStorage.setItem(dog breed, data about dog) this will be the same for the cat Api
 // use localStorge.getItem() to load breed when the user clicks previous/next breed
-document.getElementById('breed').addEventListener("change", onSelectChange)
-// boxer
-// dalmation
-// Greyhound
-// Golden Retreiver
-// Great Dane
+document.getElementById('breed').addEventListener("change", onDogChange)
+document.getElementById('breed').addEventListener("change", onCatChange)
+
 
 
 // placeholder parameter. drop down menu will populate this to call the dogApi on the selected breed from webpage
@@ -80,11 +78,8 @@ document.getElementById('breed').addEventListener("change", onSelectChange)
             // use breed ID to create url to fetch image X
     // fetch cat images X
         // append images onto html webpage
-function catPicture() {
-    // match element to html, 'beng' is a placeholder
-    var catID = document.getElementById('Beng').textContent;
-    var searchID = catID.toLowerCase().slice(0, 4); 
-    var catUrl = 'https://api.thecatapi.com/v1/images/search?breed_ids=' + searchID;
+function catPicture(value) {
+    var catUrl = 'https://api.thecatapi.com/v1/images/search?breed_ids=' + value;
     console.log(catUrl);
     fetch(catUrl)
     .then (function (response) {
@@ -98,26 +93,19 @@ function catPicture() {
        imageElement.classList.add("cat-image");
        imageContainer.appendChild(imageElement);
        document.body.appendChild(imageContainer);
-      catInfo();
     })
 }
-function catInfo() {
-    var catID = document.getElementById('Beng').textContent;
-    var searchID = catID.toLowerCase().slice(0, 4); 
-    var breedURL = 'https://api.thecatapi.com/v1/breeds/' + searchID;
+function catInfo(value) {
+    var breedURL = 'https://api.thecatapi.com/v1/breeds/' + value;
     console.log(breedURL);
     fetch(breedURL)
     .then (function (response) {
         return response.json();
     })
     .then (function (data) {
-        console.log(data);
         var catDescription = data.description;
-        console.log(catDescription);
         var catAttitude = data.temperament;
-        console.log(catAttitude);
         var catLife = data.life_span;
-        console.log(catLife);
         var infoContainer = document.createElement("div");
         infoContainer.classList.add("cat-info");
         var descriptionPara = document.createElement("p");
@@ -132,7 +120,7 @@ function catInfo() {
     })
 }
 // use breedURL to fetch description and life span X
-catPicture();
+// catPicture();
 // bengal id beng
 // abyssinian id abys   
 // bombay id bomb
