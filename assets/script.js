c987e403fd0d17c369928e9bc83894db06a1c77c
Cole Hartkopp <chsoccer4@outlook.com>
2023-01-31 13:12:12 -0600

    updated README, removed comments from js, added screenshots of webpage

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,3 @@
-
-// cat API Key live_DumA8PS0G2lXkKvpVOVCuqMLTBJB0qi7ZuWTd6Tf4wqEPwHCDLOZxjHSzdx3UU5p
-// var selectedBreed = document.getElementById('breed').value;
-// create api call for dog images X
-    // fetch dog images X
-        // append images onto html webpage X
-
 function dogImage(value) {
     // value is input from html of what breed the user selects from drop down menu
     var requestUrl = 'https://dog.ceo/api/breed/' + value + '/images/random';
@@ -25,8 +18,6 @@ function dogImage(value) {
             // document.body.appendChild(imageContainer);
         })
 }     
-// {55: 'Boxer',92:'Dalmatian', 121:'Golden Retriever', 124:'Great Dane', 127:'Greyhound'}
-// data value = # for breed, needs to be set for each dropdown item
 function dogApi(value) {
     var dogUrl = 'https://api.thedogapi.com/v1/breeds/' + value;
     console.log(dogUrl);
@@ -45,7 +36,6 @@ function dogApi(value) {
        var lifePara = document.createElement("p");
        lifePara.textContent = "Life Span: " + dogLife;
        infoContainer.appendChild(lifePara);
-    //    document.body.appendChild(infoContainer);
     })
     
 }
@@ -75,25 +65,9 @@ function onCatChange(event) {
     catInfo(event.target.value);
 }
 
-// return response.textContent = 'information we want to pull from dog API'
-// need to make it so when the data is retrieved it is displayed at the bottom of the HTML doc, line 99 textArea replace placeholder
-// use local storage to store information that is retrieved from dog API. 
-// localStorage.setItem(dog breed, data about dog) this will be the same for the cat Api
-// use localStorge.getItem() to load breed when the user clicks previous/next breed
 document.getElementById('dogBreed').addEventListener("change", onDogChange)
 document.getElementById('catBreed').addEventListener("change", onCatChange)
 
-
-
-// placeholder parameter. drop down menu will populate this to call the dogApi on the selected breed from webpage
-// dogImage('hound');
-
-// create api call for cat images X
-    // fetch cat api data X
-        // grab the cad breeds breed ID X
-            // use breed ID to create url to fetch image X
-    // fetch cat images X
-        // append images onto html webpage
 function catPicture(value) {
     var catUrl = 'https://api.thecatapi.com/v1/images/search?breed_ids=' + value;
     console.log(catUrl);
@@ -133,15 +107,7 @@ function catInfo(value) {
         infoContainer.appendChild(attitudePara);
     })
 }
-// use breedURL to fetch description and life span X
-// catPicture();
-// bengal id beng
-// abyssinian id abys   
-// bombay id bomb
-// chartreux id char
-// oriental id orie
-
-// modal for dog button
+// modal for chosen breed
 document.addEventListener('DOMContentLoaded', () => {
     // Functions to open and close a modal
     function openModal($el) {
